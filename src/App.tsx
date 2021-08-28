64358e0c0d248499ac7af1bddaa29f4a8a1e42e8
azharimm <azhari.marzan@gmail.com>
2021-08-28 13:34:34 +0700

    add react-router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//Components
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Owned from "./pages/Owned";
+//Styles
+import "./App.css";
+import logo from './assets/logo.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="container min-h-screen min-w-full bg-green-700 opacity-80">
+				<nav className="w-full md:w-2/3 px-0 py-3 mx-auto">
+					<div className="flex justify-between items-center mx-5 md:mx-0">
+						<h1 className="text-white text-xl">
+							<Link to="/" className="flex">
+								<img
+									src={logo}
+									alt="logo"
+									className="flex-1 w-7 mr-1"
+								/>
+								<span>Pokedex</span>
+							</Link>
+						</h1>
+						<div className="flex items-center text-white text-xs">
+							<Link
+								to="/detail"
+								className="px-1 py-2 mr-2 w-20 sm:w-20 flex justify-center bg-green-600 rounded-md h-8 items-center"
+							>
+								<span className="sm:block">&nbsp;Detail</span>
+							</Link>
+							<Link
+								to="/owned"
+								className="px-1 py-2 mr-2 w-20 sm:w-20 flex justify-center bg-green-600 rounded-md h-8 items-center"
+							>
+								<span className="sm:block">&nbsp;Owned</span>
+							</Link>
+						</div>
+					</div>
+				</nav>
+				<Switch>
+					<Route path="/detail">
+						<Detail />
+					</Route>
+					<Route path="/owned">
+						<Owned />
+					</Route>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;

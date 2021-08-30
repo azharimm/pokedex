c0927e11dce1a1921a20278fd725c96458fa4e04
azharimm <azhari.marzan@gmail.com>
2021-08-30 16:26:26 +0700

    update: link to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,15 @@
-import React, { FormEvent, useCallback, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { useLazyQuery } from "@apollo/client";
-import { GET_POKEMON } from "../graphql/Queries";
+import React from "react";
+import { Link } from "react-router-dom";
 import { useStateValue } from "../context/StateProvider";
 import logo from "../assets/logo.svg";
 
 function Navbar() {
-	const history = useHistory();
-	const { dispatch } = useStateValue();
-	const [fetchPokemon, fetch] = useLazyQuery(GET_POKEMON);
-	useEffect(() => {
-		if (fetch.data) {
-			dispatch({
-				type: "FETCH_POKEMON",
-				payload: fetch.data.pokemon_v2_pokemon,
-			});
-			dispatch({
-				type: "SET_QUERY",
-				payload: "",
-			});
-			dispatch({
-				type: "SET_IS_SEARCH",
-				payload: false,
-			});
-		}
-	}, [fetch, dispatch]);
-	const handleOnClick = useCallback(
-		(e: FormEvent) => {
-			e.preventDefault();
-			fetchPokemon();
-			history.push("/");
-		},
-		[history, fetchPokemon]
-	);
 	return (
 		<nav className="w-full md:w-2/3 px-0 py-3 mx-auto">
 			<div className="flex justify-between items-center mx-5 md:mx-0">
 				<h1 className="text-white text-xl">
-					<a
-						href="/"
-						onClick={(e) => handleOnClick(e)}
+					<Link
+						to="/"
 						className="flex cursor-pointer"
 					>
 						<img
@@ -48,7 +18,7 @@ function Navbar() {
 							className="flex-1 w-7 mr-1"
 						/>
 						<span>Pokedex</span>
-					</a>
+					</Link>
 				</h1>
 				<div className="flex items-center text-white text-xs">
 					<Link
